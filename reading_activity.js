0daf1ce5e6b3caaa8a76c1724e3eecfbccd15fa2
Mary Skitka <mary.skitka@tufts.edu>
2016-09-27 10:00:51 -0400

    added socket connection

--- reading_activity.js
+++ reading_activity.js
@@ -1,8 +1,23 @@
+	
 	window.onload = function(){
 		$("#flipbook").turn({
 			width: 1000,
 			height: 600,
 		});
+		connect();
 	}
+
+function connect(){
+	var ws = new WebSocket("ws://localhost:9998/echo");//ws (websocket)
+	console.log(ws.readyState);
+	ws.onopen = function(){
+		console.log("websocket connection is opened");
+	};
+	ws.onmessage = function (evt){
+		var received_msg = evt.data;
+		alert("stop spacing out, you goob!");
+	}
+
+}
 
 
